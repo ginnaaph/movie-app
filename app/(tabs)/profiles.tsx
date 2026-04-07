859cc0e5b98aba69ac4e3e3ebb73199a7cd26409
ginnaaph <161983228+ginnaaph@users.noreply.github.com>
2026-04-07 12:46:37 -0700

    scaffold saved page

--- app/(tabs)/profiles.tsx
+++ app/(tabs)/profiles.tsx
@@ -1,14 +1,16 @@
 import { icons } from "@/constants/icon";
+import { images } from "@/constants/images";
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 
 const Profiles = () => {
   return (
     <View className="bg-primary flex-1 px-10">
+      <Image source={images.navybg} style={StyleSheet.absoluteFillObject} />
       <View className="flex justify-center items-center flex-1 flex-col gap-5">
-        <Image source={icons.profile} className="size-10" tintColor="#000" />
+        <Image source={icons.profile} className="size-10" tintColor="#fff" />
 
-        <Text className="text-slate-700"> profiles</Text>
+        <Text className="text-white"> Profiles</Text>
       </View>
     </View>
   );
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,14 +1,29 @@
+import { icons } from "@/constants/icon";
+import { images } from "@/constants/images";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const Saved = () => {
   return (
-    <View>
-      <Text>Saved</Text>
+    <View className="bg-primary flex-1 px-10">
+      <Image source={images.navybg} style={StyleSheet.absoluteFillObject} />
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.saved} className="size-10" tintColor="#fff" />
+
+        <Text className="text-white"> Saved</Text>
+      </View>
     </View>
   );
 };
 
 export default Saved;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  movieIcon: {
+    width: 48,
+    height: 40,
+    marginTop: 80,
+    marginBottom: 20,
+    alignSelf: "center",
+  },
+});
