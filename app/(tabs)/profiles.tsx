466b653e6112f20b49239502d7c147e6de12d58f
ginnaaph <161983228+ginnaaph@users.noreply.github.com>
2026-04-07 12:39:51 -0700

    scaffold profiles page

--- app/(tabs)/profiles.tsx
+++ app/(tabs)/profiles.tsx
@@ -1,10 +1,15 @@
+import { icons } from "@/constants/icon";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const Profiles = () => {
   return (
-    <View>
-      <Text>profiles</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.profile} className="size-10" tintColor="#000" />
+
+        <Text className="text-slate-700"> profiles</Text>
+      </View>
     </View>
   );
 };
